b70d8647282a3aa430a48a1457af67215fcc5e2c
Elena Fedchenko <fedchenko.yelena@gmail.com>
2025-02-10 12:35:44 +1300

    Initial Commit

---
+++ Workout Count/Workout_CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_CountApp.swift
+//  Workout Count
+//
+//  Created by Elena on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Workout_CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Workout CountTests/Workout_CountTests.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_CountTests.swift
+//  Workout CountTests
+//
+//  Created by Elena on 10/02/2025.
+//
+
+import Testing
+@testable import Workout_Count
+
+struct Workout_CountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
